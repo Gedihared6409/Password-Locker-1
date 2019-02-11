0102cb4ca08f0cfa7f2bf530e4e8e9e0a40a67c0
kayitesijackie <bazimyas@gmail.com>
2019-02-11 13:07:39 +0200

    add save_user method

--- user.py
+++ user.py
@@ -7,4 +7,10 @@ class User:
     def __init__(self,username,password):
         self.username = username
         self.password = password
+    def save_user(self):
+        """
+        save_user method saves user objects into the user_list
+        """
+        User.user_list.append(self)
+
     
