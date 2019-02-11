43f9789746fc5799d514282faaab7f011324136d
kayitesijackie <bazimyas@gmail.com>
2019-02-11 14:32:36 +0200

    add _init_ method and array credential_list

--- credentials.py
+++ credentials.py
@@ -3,3 +3,11 @@ class Credential:
     """
     Class that generates new instances of user's credentials
     """
+
+    credential_list=[]
+
+    def __init__(self,view_password,account,login,password):
+        self.view_password = view_password
+        self.account = account
+        self.login = login
+        self.password = password
