3f18bea7828799ffc1c13cec67557d2475b81761
kayitesijackie <bazimyas@gmail.com>
2019-02-11 12:31:26 +0200

    import unittest framework and User class

---
+++ test-user.py
@@ -0,0 +1,8 @@
+import unittest # Importing the unittest module
+from user import User # Importing the user class
+
+class TestUser(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the contact class behaviours.
+    '''
