a5a8e90c8491015ed19cad9f09398972e98b79de
kayitesijackie <bazimyas@gmail.com>
2019-02-11 14:45:24 +0200

    add TestCredential class

--- test-credentials.py
+++ test-credentials.py
@@ -1,2 +1,12 @@
 import unittest
 from credentials import Credential
+
+class TestCredential(unittest.TestCase):
+    """
+    Test class that defines test cases for the credential class behaviours
+    """
+    def setUp(self):
+        """
+        set up method to run before each test cases
+        """
+        self.new_credential = Credential("123456","twitter","nay","12345")
