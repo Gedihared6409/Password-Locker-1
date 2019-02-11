7d35a756b0eb9a42a92fa06a2e851fd19b30f9ae
kayitesijackie <bazimyas@gmail.com>
2019-02-11 15:41:34 +0200

    add create_credentials function

--- run.py
+++ run.py
@@ -8,4 +8,10 @@ def create_user_account(user_name,pass_word):
     """
     new_user = User(user_name,pass_word)
     return new_user
-    
+
+def create_credentials(view_password,account,login_name,pass_word):
+    """
+    Function to create a new credential
+    """
+    new_credential = Credential(view_password,account,login_name,pass_word)
+    return new_credential
