1842fbe07b2d6c56de3cb399a75be2ba95bbde5e
kayitesijackie <bazimyas@gmail.com>
2019-02-11 12:10:26 +0200

    create an array user_list

--- user.py
+++ user.py
@@ -2,3 +2,8 @@ class User:
     """
     Class that generates new instances of users
     """
+    user_list=[]
+
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
