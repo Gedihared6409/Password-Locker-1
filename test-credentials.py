ce85341944a0529a16feda3faa09d5acd546dea3
kayitesijackie <bazimyas@gmail.com>
2019-02-11 15:03:21 +0200

    add tearDown method and Credential_list array

--- test-credentials.py
+++ test-credentials.py
@@ -10,3 +10,9 @@ class TestCredential(unittest.TestCase):
         set up method to run before each test cases
         """
         self.new_credential = Credential("0788312907jk","twitter","kayitesijackie","0788312907jk")
+
+    def tearDown(self):
+        """
+        tearDown method that does clean up after each test case has run
+        """
+        Credential.credential_list = []
